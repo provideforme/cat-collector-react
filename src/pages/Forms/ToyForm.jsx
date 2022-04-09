7b8e364c71d9cb1247f8b6bf8c37b56d486f3b01
provideforme <evan.a.bernard@gmail.com>
2022-04-08 21:55:58 -0400

    update toy

--- src/pages/Forms/ToyForm.jsx
+++ src/pages/Forms/ToyForm.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
 // Services
+import { getOne } from '../../services/toys'
 
 
 // Components
@@ -26,7 +27,19 @@ const ToyForm = (props) => {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  useEffect(() => {}, [id])
+  useEffect(() => {
+    const fetchOne = async () => {
+      const toyData = await getOne(id)
+      setForm({
+        id: toyData.id,
+        name: toyData.name,
+        color: toyData.color,
+        description: toyData.description,
+      })
+    }
+    id ? fetchOne() : setForm({ color: '#ff0000' })
+    return () => setForm({})
+  }, [id])
 
   return (
     <>
